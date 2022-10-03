3bb4b58820e4afe731e886dff91ff5f968dd7f0a
AJ Domingo <domingo.aj.888@gmail.com>
2022-10-03 16:44:38 -0500

    add final code

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,29 +2,62 @@ import os
 import csv
 
 budgetcsv = os.path.join('Resources', 'budget_data.csv')
-findata = open(budgetcsv)
-output_path = os.path.join('Analysis', 'analysis.txt')
+output = os.path.join('Analysis', 'analysis.txt')
 
-with findata as fin:
+pl_change = []
+date = []
+profit = []
+
+open_profit = 0
+months = 0
+
+with open(budgetcsv) as fin:
     finread = csv.reader(fin, delimiter=',')
 
-    headerline = next(fin)
+    headerline = next(finread)
 
-    total = 0
-    months = len(list(fin))
-    
     for row in finread:
-        total += int(row[1])
+        months = months + 1
 
-    avgchange = (total/months)
-    
-    print("Financial Analysis")
-    print("---------------------")
-    print(f"Total Months: {months}")
-    print(f"Total: ${total}")
-    print(f"Average Change: ${avgchange}")
-    print(f"Greatest Increase in Profits: {greatdate} ({greatnum})")
-    print(f"Greatest Decrease in Profits: {baddate} ({badnum})")
+        month_profit = int(row[1])
+        month_change = month_profit - open_profit
 
+        profit.append(month_profit)
+        date.append(row[0])
+        pl_change.append(month_change)
 
+        open_profit = month_profit
 
+remove_first_prof = pl_change.pop(0)
+remove_first_date = date.pop(0)
+
+pl_info = zip(date, pl_change)
+total_change = sum(pl_change)
+total_profit = sum(profit)
+change_avg = float(total_change / len(pl_change))
+
+greatnum = max(pl_change)
+badnum = min(pl_change)
+
+for month, change in pl_info:
+    if change == greatnum:
+        greatdate = month
+    if change == badnum:
+        baddate = month
+
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {months}")
+print(f"Total: ${total_change}")
+print(f"Average Change: ${change_avg:.2f}")
+print(f"Greatest Increase in Profits: {greatdate} ({greatnum})")
+print(f"Greatest Decrease in Profits: {baddate} ({badnum})")
+
+with open(output, "w") as txtfile:
+    txtfile.write("Financial Analysis\n")
+    txtfile.write("---------------------\n")
+    txtfile.write("Total Months:" + str(months) + "\n")
+    txtfile.write("Total: $" + str(total_change) + "\n")
+    txtfile.write("Average Change: $" + str('{:0.2f}'.format(change_avg)) + "\n")
+    txtfile.write("Greatest Increase in Profits:" + str(greatdate) + "(" + str(greatnum) + ")" + "\n")
+    txtfile.write("Greatest Decrease in Profits:" + str(baddate) + "(" + str(badnum) + ")" + "\n")
